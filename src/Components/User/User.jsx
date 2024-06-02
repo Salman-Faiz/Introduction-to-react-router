22f5be0880e0c48c9ec2345ba2949931826d154b
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-06-02 10:47:04 +0600

    useNavigate hook to handle onclick btn

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const User = ({user}) => {
     const {id,name,email,username,address,phone} =user;
+    const navigate= useNavigate();
+    const handleDetails =()=>{
+        navigate(`/users/${id}`);
+    }
     return (
         <div className="bg-slate-400 border-2 rounded-lg p-4"> 
            
@@ -12,6 +16,7 @@ const User = ({user}) => {
             <h1>{phone}</h1>
             <h1>{address.street}</h1>
             <Link to={`/users/${id}`}><button className="bg-blue-500 text-2xl w-full rounded-lg">show details</button></Link>
+            <button className="bg-green-600 w-full mt-4 rounded-md" onClick={handleDetails} >onClick btn</button>
         </div>
     );
 };
